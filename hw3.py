4b941a99bcd4c4e6042b986d36965ace3ac4a0bc
Daniel Dukeshire <dukesd3@rpi.edu>
2020-10-27 18:42:30 -0400

    Setting up the threading

--- hw3.py
+++ hw3.py
@@ -20,23 +20,33 @@ import threading
 import csci4220_hw3_pb2
 import csci4220_hw3_pb2_grpc
 
+# Some global variables
+N = 4                                   # The maximum number of buckets (N) is 4
+buckets = [[]] * N                      # The k_buckets array
+
 # gatherCommandLine()
 # reads-in input from the command-line in the form of:
 # <nodeID> <portnum> <k>
 def gatherCommandLine():
-	if len(sys.argv) != 4:
-		print("Error, correct usage is {} [my id] [my port] [k]".format(sys.argv[0]))
-		sys.exit(-1)
+    if len(sys.argv) != 4:                          # Some error checking
+        print("Error, correct usage is {} [my id] [my port] [k]".format(sys.argv[0]))
+        sys.exit(-1)
+    global local_id
+    global my_port
+    global my_hostname
+    global my_address
 
-	local_id = int(sys.argv[1])
-	my_port = str(int(sys.argv[2]))                                                             # add_insecure_port() will want a string
-	k = int(sys.argv[3])
-	my_hostname = socket.gethostname()                                                          # Gets my host name
-	my_address = socket.gethostbyname(my_hostname)                                              # Gets my IP address from my hostname
+    local_id = int(sys.argv[1])
+    my_port = str(int(sys.argv[2]))                  # add_insecure_port() will want a string
+    k = int(sys.argv[3])
+    my_hostname = socket.gethostname()               # Gets my host name
+    my_address = socket.gethostbyname(my_hostname)   # Gets my IP address from my hostname
 
-def initializingBuckets():
+def connectionListen():
+    print("gRPC server starting at: {}".format(my_address+':'+my_port))
 
-
+# main
 if __name__ == '__main__':
-	gatherCommandLine()
-    initializingBuckets()
+    gatherCommandLine()
+    server = threading.Thread(target = connectionListen)    # server thread, so we can simultaniously do ....
+    server.start()
